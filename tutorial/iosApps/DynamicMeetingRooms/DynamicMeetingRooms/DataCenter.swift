56ba0276fbf949d77ec46e0dc4a53a77debf51b0
Jaewon Kim <jaewonism57@gmail.com>
2016-05-26 00:43:04 +0900

    네비게이션 실습

---
+++ tutorial/iosApps/DynamicMeetingRooms/DynamicMeetingRooms/DataCenter.swift
@@ -0,0 +1,66 @@
+//
+//  DataCenter.swift
+//  DynamicMeetingRooms
+//
+//  Created by 김재원 on 2016. 5. 25..
+//  Copyright © 2016년 Jaewonism. All rights reserved.
+//
+
+import Foundation
+let dataCenter: DataCenter = DataCenter()
+
+class DataCenter {
+    var branches: [Branch] = []
+    init () {
+        let banksyRoom = MeetingRoom(name: "Banksy", capacity: 4)
+        let kahloRoom = MeetingRoom(name: "Kahlo", capacity: 8)
+        let riveraRoom = MeetingRoom(name: "Rivera", capacity: 8)
+        let picassoRoom = MeetingRoom(name: "Picasso", capacity: 10)
+        
+        let vehicleService = Service(name: "차량 예약")
+        let meetingRoomService = Service(name: "회의실 예약")
+        let visitorService = Service(name: "방문자 예약")
+        let deskService = Service(name: "데스크 예약")
+        
+        meetingRoomService.items = [banksyRoom, kahloRoom, riveraRoom, picassoRoom]
+        
+        let pangyoBranch = Branch(name: "판교점")
+        let samsungBranch = Branch(name: "삼성점")
+        let yeoksamBranch = Branch(name: "역삼점")
+        let sinrimBranch = Branch(name: "신림점")
+        let songdoBranch = Branch(name: "송도점")
+        let anamBranch = Branch(name: "안암점")
+        
+        pangyoBranch.services = [vehicleService, meetingRoomService, visitorService, deskService]
+        
+        branches += [pangyoBranch, samsungBranch, yeoksamBranch, sinrimBranch, songdoBranch, anamBranch]
+    }
+}
+
+class Branch {
+    let name: String
+    var services: [Service]?
+    
+    init (name: String) {
+        self.name = name
+    }
+}
+
+class Service {
+    let name: String
+    var items: [MeetingRoom]?
+    
+    init (name: String) {
+        self.name = name
+    }
+}
+
+class MeetingRoom {
+    let name: String
+    let capacity: Int
+    
+    init (name: String, capacity: Int) {
+        self.name = name
+        self.capacity = capacity
+    }
+}
